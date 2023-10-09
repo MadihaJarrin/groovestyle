62ae0827612caf5b81f3e6e2698e53c6f33487ad
MadihaJarrin <madihajarrin1@gmail.com>
2023-10-09 06:14:41 +0600

    added

--- src/Pages/Categories/AllCategories/AllCategories.jsx
+++ src/Pages/Categories/AllCategories/AllCategories.jsx
@@ -1,17 +1,35 @@
 import ListItems from "../../Home/Home/Shared/ListItems/ListItems";
+import ParallaxCover from "../../Home/Home/Shared/ParallaxCover/ParallaxCover";
 
-const AllCategories = ({ items }) => {
+const AllCategories = ({ items, title, img }) => {
     return (
-        <div>
-            <div className="grid md:grid-cols-2 gap-20 p-20">
+        // <div>
+        //     <div className="grid md:grid-cols-2 gap-20 p-20">
+        //         {title && <ParallaxCover img={img} title={title}></ParallaxCover>}
+
+        //         {
+        //             items.map(item => <ListItems
+        //                 key={item._id}
+        //                 item={item} //send item to listitems
+        //             ></ListItems>)
+        //         }
+
+        //         <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+
+        //     </div>
+        // </div>
+        <div className='p-20'>
+            {title && <ParallaxCover img={img} title={title}></ParallaxCover>}
+            <div className="grid md:grid-cols-2 gap-10 my-16">
                 {
                     items.map(item => <ListItems
                         key={item._id}
-                        item={item} //send item to listitems
+                        item={item}
                     ></ListItems>)
                 }
-                <button className="btn btn-outline border-0 border-b-4 mt-4">View All Collections</button>
             </div>
+            <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+
         </div>
     );
 };
--- src/Pages/Categories/Categories/Categories.jsx
+++ src/Pages/Categories/Categories/Categories.jsx
@@ -4,12 +4,14 @@ import AllCategories from '../AllCategories/AllCategories';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import ParallaxCover from '../../Home/Home/Shared/ParallaxCover/ParallaxCover';
 import CategoriesImg from '../../../assets/Categories/Cover/categoriesCover4.jpg'
-
+import shoesImg from '../../../assets/Categories/Cover/shoescover.jpg'
+import dressImg from '../../../assets/Categories/Cover/dresscover.jpg'
+import bagsImg from '../../../assets/Categories/Cover/bagscover.jpg'
 const Categories = () => {
     const [list] = useCategories()
-    // const shoes = list.filter(item => item.category === 'shoes');
-    // const dress = list.filter(item => item.category === 'dress');
-    // const bags = list.filter(item => item.category === 'bags');
+    const shoes = list.filter(item => item.category === 'shoes');
+    const dress = list.filter(item => item.category === 'dress');
+    const bags = list.filter(item => item.category === 'bags');
     const offered = list.filter(item => item.category === 'offered');
 
     return (
@@ -17,15 +19,16 @@ const Categories = () => {
             <Helmet>
                 <title>Groovestyle | Categories</title>
             </Helmet>
-            <ParallaxCover img={CategoriesImg} title="All Categories" ></ParallaxCover>
-            <SectionTitle
-                heading="Offered Items"
-                subHeading="5% discount on"
-            >
-            </SectionTitle>
-            <AllCategories
-                items={offered}
-            ></AllCategories>
+
+            <ParallaxCover img={CategoriesImg} title="our all product"></ParallaxCover>
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items */}
+            <AllCategories items={offered}></AllCategories>
+            {/* dessert menu items  */}
+            <AllCategories items={shoes} title="shoes" img={shoesImg}></AllCategories>
+            <AllCategories items={dress} title={"pizza"} img={dressImg}></AllCategories>
+            <AllCategories items={bags} title={"salad"} img={bagsImg}></AllCategories>
         </div>
     );
 };
